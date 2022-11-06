36575e6b477d25c881be6aa81e55ecee8efa4aff
Krishanu <krishanu.info@gmail.com>
2022-11-07 03:56:04 +0530

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,12 @@
 
     <div class="flex justify-center items-center h-screen">
 
-        <img height="80px" width="60px" src="@img(storage/images.jpg)">
+        <img height="80px" width="60px" src="@img(storage/images.jpg)" alt="image file">
 
 
         <h1 class="text-3xl text-purple-600 font-bold">Welcome</h1>
 
-        <img height="80px" width="60px" src="@img(storage/images.jpg)">
+        <img height="80px" width="60px" src="@img(storage/images.jpg)" alt="image file">
     </div>
 
 
