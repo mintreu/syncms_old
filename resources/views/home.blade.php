f91c9e5019019658e982d3a564241e545ac5863f
Krishanu <krishanu.info@gmail.com>
2022-11-06 15:13:08 +0530

    re init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<x-layout::layout>
+
+
+
+
+
+    <div class="flex justify-center items-center h-screen">
+
+        <img height="80px" width="60px" src="@img(storage/images.jpg)">
+
+
+        <h1 class="text-3xl text-purple-600 font-bold">How to Install Tailwind CSS 3 in Laravel 9 With Vite 3 - TechvBlogs</h1>
+
+        <img height="80px" width="60px" src="@img(storage/images.jpg)">
+    </div>
+
+
+</x-layout::layout>
+
